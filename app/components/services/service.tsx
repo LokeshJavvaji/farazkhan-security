b04dd82499e4f49dad0607ce0496731e8898b6e6
Lokesh Javvaji <83934051+LokeshJavvaji@users.noreply.github.com>
2025-03-25 11:50:30 +0530

    Update service.tsx

--- app/components/services/service.tsx
+++ app/components/services/service.tsx
@@ -8,7 +8,7 @@ export const ServicesPage = ({ data }: { data: { type: string; image: string; vi
     return (
         <main>
             <section>
-                <div className="relative w-full h-full">
+                <div className="relative w-full h-[90vh]">
                     <Image
                         src={data[0].image}
                         alt="VPN security application on laptop and smartphone"
